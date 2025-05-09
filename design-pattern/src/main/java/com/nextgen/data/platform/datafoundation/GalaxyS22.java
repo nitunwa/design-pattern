fcb57c0df16f8e01b9847e8962b359459524367a
nitunwa <nitun.2@gmail.com>
2025-05-08 21:04:35 -0700

    Delete design-pattern/src/main/java/com/nextgen/data/platform/datafoundation directory

--- design-pattern/src/main/java/com/nextgen/data/platform/datafoundation/GalaxyS22.java
+++
@@ -1,8 +0,0 @@
-package com.nextgen.data.platform.datafoundation;
-
-public class GalaxyS22 implements Phone{
-    @Override
-    public String description() {
-        return "Phone type: GalaxyS22";
-    }
-}
