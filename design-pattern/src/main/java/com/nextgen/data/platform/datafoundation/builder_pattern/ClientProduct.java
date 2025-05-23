c7387703ebda4e9d1743b2ce61c46c6af5815f6d
sujan kishor nath <snath@sujans-MacBook-Pro.local>
2025-05-23 10:26:37 -0700

    add commit

--- design-pattern/src/main/java/com/nextgen/data/platform/datafoundation/builder_pattern/ClientProduct.java
+++ design-pattern/src/main/java/com/nextgen/data/platform/datafoundation/builder_pattern/ClientProduct.java
@@ -1,2 +1,16 @@
-package com.nextgen.data.platform.datafoundation.builder_pattern;public class ClientProduct {
+package com.nextgen.data.platform.datafoundation.builder_pattern;
+
+
+public class ClientProduct {
+    public static void main(String[] args) {
+        Product product1 = new Product.ProductBuilder()
+                .withName("iPhone 15")
+                .withDescription("Latest Apple smartphone")
+                .withPrice(1099.99)
+                .withCategory("Electronics")
+                .withRating(4.8)
+                .Build();
+
+        System.out.println(product1);
+    }
 }
